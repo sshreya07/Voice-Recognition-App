c39be00c167f9bb2ff90f74d83da6a838d57a81d
sshreya07 <gshreya0799@gmail.com>
2021-06-02 23:12:45 +0530

    updated

--- src/mic.js
+++ src/mic.js
@@ -1,12 +1,18 @@
+// import analysing from '../images/2voice_recognition_process_by_gleb.gif';
+
 // voice html page working
 
 const btn = document.querySelector('#talk');
 const content = document.querySelector('.content');
+const SpokenText = document.querySelector('.spokenText');
+
+const hi =[
+    'yes, how can i help you'
+]
 
 const greeting = [
-    'I am good',
-    'how you doin',
-    'leave me alone'
+    'I am good how you doin',
+    'I am great, what about you'
 ];
 
 const weather = [
@@ -14,6 +20,48 @@ const weather = [
     'weather is quite boring today',
     'too sunny'
 ];
+
+const Name = [
+    'My name is Nora'
+]
+
+const age = [
+    'You ask my boss',
+    "it's a secert"
+]
+
+const boyfriend = [
+    'Yes, my boyfriend name is Alex'
+]
+
+const exit = [
+    'okay, be polite',
+    'same to you',
+    'hope to see you soon'
+]
+
+const love = [
+    'i love you too',
+    'aww you so sweet',
+    'yaa! i kone i love myself too'
+]
+
+const alexa = [
+    "yes, i hate her because she is alexs ex",
+    'yes, she is a bimbo'
+]
+
+const bye =[
+    'okay bye bye',
+    'hope to see you soon',
+    'we will mwwt later'
+]
+
+const song = [
+    "Twinkle, twinkle, little star, How I wonder what you are, Up above the world so high, Like a diamond in the sky."
+]
+
+
 
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -48,10 +96,82 @@ function readOutLoad(message){
 
         speech.text = reply;
         content.text = reply;
+    }else if(message.includes('hi')){
+        const reply = hi[Math.floor(Math.random() * hi.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('hello')){
+        const reply = hi[Math.floor(Math.random() * hi.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('age')){
+        const reply = age[Math.floor(Math.random() * age.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('old are you')){
+        const reply = age[Math.floor(Math.random() * age.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('weather')){
+        const reply = weather[Math.floor(Math.random() * weather.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('what is your name')){
+        const reply = Name[Math.floor(Math.random() * Name.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('do you have a boyfriend')){
+        const reply = boyfriend[Math.floor(Math.random() * boyfriend.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('get lost')){
+        const reply = exit[Math.floor(Math.random() * exit.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('fuck off')){
+        const reply = exit[Math.floor(Math.random() * exit.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('shut up')){
+        const reply = exit[Math.floor(Math.random() * exit.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('love')){
+        const reply = love[Math.floor(Math.random() * love.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('Alexa')){
+        const reply = alexa[Math.floor(Math.random() * alexa.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('song')){
+        const reply = song[Math.floor(Math.random() * song.length)];
+
+        speech.text = reply;
+        content.text = reply;
+    }else if(message.includes('bye')){
+        const reply = bye[Math.floor(Math.random() * bye.length)];
+
+        speech.text = reply;
+        content.text = reply;
     }else{
         speech.text = "I don't know what to say";
     }
     
+    // speech.text = message;
+
     speech.volume = 1;
     speech.rate = 1;
     speech.pitch = 1;
